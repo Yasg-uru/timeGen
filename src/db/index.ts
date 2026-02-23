46d6f7f2f0e9f06e35573f0c6a35404d39881f8a
yash choudhary <yashpawar12122004@gmail.com>
2026-02-23 21:31:40 +0530

    changes

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import { logger } from '../utils/logger';
-const MONGO_URI = 'mongodb+srv://yashpawar12122004:KaeUSgv4Letln7rX@crushsphere-location-se.zu98e.mongodb.net/?retryWrites=true&w=majority&appName=crushsphere-location-service';
+// const MONGO_URI = 'mongodb+srv://yashpawar12122004:KaeUSgv4Letln7rX@crushsphere-location-se.zu98e.mongodb.net/?retryWrites=true&w=majority&appName=crushsphere-location-service';
+const MONGO_URI = "mongodb+srv://yash26it074_db_user:eG6vYfetY2IszUD0@cluster0.j3qnxhy.mongodb.net/?appName=Cluster0";
 export async function connectDB() {
   try {
     await mongoose.connect(MONGO_URI);
